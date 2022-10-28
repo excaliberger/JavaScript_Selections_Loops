238f0d3c76fe85e8a463a9dd583ff8230c57596b
Emily Berger <excaliberger@gmail.com>
2022-10-28 16:35:11 -0400

    exercise 2

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ for (let counter = 1; counter <= 20; counter++) {
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+for (let counter = 1; counter <= 100; counter++) {
+    if (counter % 3 === 0 && counter % 5 === 0) {
+        console.log("fizzbuzz");
+    } else if (counter % 5 === 0) {
+        console.log("buzz");
+    } else if (counter % 3 === 0) {
+        console.log("fizz");
+    } else {
+        console.log(counter)
+    }
+}
+
