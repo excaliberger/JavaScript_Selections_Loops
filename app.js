de32b736e7a016f64785d04bc73dfcd41510499d
Emily Berger <excaliberger@gmail.com>
2022-10-28 17:01:26 -0400

    exercise 3

--- app.js
+++ app.js
@@ -3,26 +3,75 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n=========\n");
 
-for (let counter = 1; counter <= 20; counter++) {
-    if (counter % 2 == 0) {
-        console.log(counter);        
-    } else {
-        continue;
-    }
-}   
+// for (let counter = 1; counter <= 100; counter++) {
+//     if (counter % 2 == 1) {
+//         console.log(counter);        
+//     } else {
+//         continue;
+//     }
+// }   
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
-for (let counter = 1; counter <= 100; counter++) {
-    if (counter % 3 === 0 && counter % 5 === 0) {
-        console.log("fizzbuzz");
-    } else if (counter % 5 === 0) {
-        console.log("buzz");
-    } else if (counter % 3 === 0) {
-        console.log("fizz");
-    } else {
-        console.log(counter)
-    }
-}
+// for (let counter = 1; counter <= 100; counter++) {
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log("fizzbuzz");
+//     } else if (counter % 5 === 0) {
+//         console.log("buzz");
+//     } else if (counter % 3 === 0) {
+//         console.log("fizz");
+//     } else {
+//         console.log(counter)
+//     }
+// }
 
+console.log("EXERCISE 3:\n==========\n");
+
+// let counter = 0
+
+// while (counter <= 100) {
+//     counter++
+//     if (counter % 2 == 1) {
+//         console.log(counter);        
+//     } else {
+//         continue;
+//     }
+// }
+
+// let counter = 0
+// do {
+//     counter++
+//     if (counter % 2 == 1) {
+//         console.log(counter);        
+//     } else {
+//         continue;
+//     }
+// } while (counter <= 100);
+
+
+// while (counter <= 100) {
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log("fizzbuzz");
+//     } else if (counter % 3 === 0) {
+//         console.log("fizz");
+//     } else if (counter % 5 === 0) {
+//         console.log("buzz")
+//     }
+//     counter++
+// }
+
+// let counter = 1
+
+// do {
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log("fizzbuzz");
+//     } else if (counter % 3 === 0) {
+//         console.log("fizz");
+//     } else if (counter % 5 === 0) {
+//         console.log("buzz")
+//     } else {
+//         console.log(counter);
+//     }
+//     counter++
+// } while (counter <= 100);
